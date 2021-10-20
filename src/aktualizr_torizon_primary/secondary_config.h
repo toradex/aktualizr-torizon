ffbb479ab7e8544e2714b1d6d5d89bd54217d69a
Patti Vacek <pattivacek@gmail.com>
2021-10-20 17:20:31 +0200

    UPSTREAM: Fixes from running clang-tidy on the headers.
    Signed-off-by: Patti Vacek <pattivacek@gmail.com>

--- src/aktualizr_torizon_primary/secondary_config.h
+++ src/aktualizr_torizon_primary/secondary_config.h
@@ -1,6 +1,7 @@
 #ifndef SECONDARY_CONFIG_H_
 #define SECONDARY_CONFIG_H_
 
+#include <string>
 #include <unordered_map>
 
 #include <json/json.h>
@@ -26,7 +27,6 @@ class IPSecondaryConfig {
     return os;
   }
 
- public:
   const std::string ip;
   const uint16_t port;
   const VerificationType verification_type;
@@ -34,7 +34,7 @@ class IPSecondaryConfig {
 
 class IPSecondariesConfig : public SecondaryConfig {
  public:
-  static const char* const Type;
+  static constexpr const char* const Type{"IP"};
   static constexpr const char* const PortField{"secondaries_wait_port"};
   static constexpr const char* const TimeoutField{"secondaries_wait_timeout"};
   static constexpr const char* const SecondariesField{"secondaries"};
@@ -47,7 +47,6 @@ class IPSecondariesConfig : public SecondaryConfig {
     return os;
   }
 
- public:
   const uint16_t secondaries_wait_port;
   const int secondaries_timeout_s;
   std::vector<IPSecondaryConfig> secondaries_cfg;
@@ -58,14 +57,19 @@ class SecondaryConfigParser {
   using Configs = std::vector<std::shared_ptr<SecondaryConfig>>;
 
   static Configs parse_config_file(const boost::filesystem::path& config_file);
+  SecondaryConfigParser() = default;
   virtual ~SecondaryConfigParser() = default;
+  SecondaryConfigParser(const SecondaryConfigParser&) = default;
+  SecondaryConfigParser(SecondaryConfigParser&&) = default;
+  SecondaryConfigParser& operator=(const SecondaryConfigParser&) = default;
+  SecondaryConfigParser& operator=(SecondaryConfigParser&&) = default;
 
   virtual Configs parse() = 0;
 };
 
 class JsonConfigParser : public SecondaryConfigParser {
  public:
-  JsonConfigParser(const boost::filesystem::path& config_file);
+  explicit JsonConfigParser(const boost::filesystem::path& config_file);
 
   Configs parse() override;
 
@@ -75,7 +79,6 @@ class JsonConfigParser : public SecondaryConfigParser {
   static void createDockerComposeSecondariesCfg(Configs& configs, const Json::Value& json_docker_compose_sec_cfg);
   // add here a factory method for another type of secondary config
 
- private:
   using SecondaryConfigFactoryRegistry = std::unordered_map<std::string, std::function<void(Configs&, Json::Value&)>>;
 
   SecondaryConfigFactoryRegistry sec_cfg_factory_registry_ = {
